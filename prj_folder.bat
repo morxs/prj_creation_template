112a0e8bdfcb523275cc8592f486bb51061a3c79
rudsu <free4spams@yahoo.com>
2015-07-22 15:17:50 +0700

    initial commit

---
+++ prj_folder.bat
@@ -0,0 +1,60 @@
+rem
+mkdir "000 - Project Management"
+mkdir "000 - Project Management\001 - Project Initiation"
+mkdir "000 - Project Management\002 - Project Work Plan"
+mkdir "000 - Project Management\003 - Overview Training & Presentation"
+mkdir "000 - Project Management\004 - Project Progress Report"
+mkdir "000 - Project Management\005 - Project Issues"
+mkdir "000 - Project Management\006 - Minutes of Meeting"
+mkdir "000 - Project Management\007 - Consultant Work Report"
+mkdir "000 - Project Management\008 - Project Change Request"
+mkdir "000 - Project Management\009 - Others"
+
+rem
+mkdir "100 - Project Preparation"
+mkdir "100 - Project Preparation\101 - Project Charter"
+mkdir "100 - Project Preparation\102 - Project Organization"
+mkdir "100 - Project Preparation\103 - Project Kick Off"
+mkdir "100 - Project Preparation\104 - Project Environment"
+mkdir "100 - Project Preparation\105 - Others"
+
+rem
+mkdir "200 - Business Blueprint"
+mkdir "200 - Business Blueprint\201 - BPML"
+mkdir "200 - Business Blueprint\202 - PDD"
+mkdir "200 - Business Blueprint\203 - Detail Requirement"
+mkdir "200 - Business Blueprint\204 - Sign Off Documents"
+mkdir "200 - Business Blueprint\205 - Others"
+
+rem
+mkdir "300 - Realization"
+mkdir "300 - Realization\301 - RICEFW"
+mkdir "300 - Realization\302 - FDS"
+mkdir "300 - Realization\303 - Technical Specification"
+mkdir "300 - Realization\304 - Configuration Scripts"
+mkdir "300 - Realization\305 - User Authorization Role"
+mkdir "300 - Realization\306 - System Integration Test"
+mkdir "300 - Realization\307 - Key User Training"
+mkdir "300 - Realization\308 - User Acceptance Test"
+mkdir "300 - Realization\309 - User Manual Guidance"
+mkdir "300 - Realization\310 - Sign Off Document"
+mkdir "300 - Realization\311 - Others"
+
+rem
+mkdir "400 - Final Preparation"
+mkdir "400 - Final Preparation\401 - End User Training"
+mkdir "400 - Final Preparation\402 - Master Data Preparation"
+mkdir "400 - Final Preparation\403 - Cutover"
+mkdir "400 - Final Preparation\404 - Sign Off Document"
+mkdir "400 - Final Preparation\405 - Others"
+
+rem
+mkdir "500 - Go Live"
+mkdir "500 - Go Live\501 - Transaction Checklist"
+mkdir "500 - Go Live\502 - Closure"
+mkdir "500 - Go Live\503 - Sign Off Document"
+mkdir "500 - Go Live\504 - Others"
+
+rem
+mkdir "600 - Handover"
+mkdir "600 - Handover\601 - Handover Checklist"
